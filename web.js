327bf5543184fff6d3b1c4e8faa06e5d919c53df
Zen2see <ubuntu@ip-172-31-30-88.us-west-2.compute.internal>
2013-07-13 06:29:23 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var app = express.createServer(express.logger());
 
 var fsrFS=fs.readFileSync('index.html', 'utf8');
 
-# app.get('/', function(request, response) {
-#  response.send('Hello World 2!');
-# });
+ app.get('/', function(request, response) {
+  response.send('Hello World 2!');
+ });
 
-console.log(fsrFS);
+#console.log(fsrFS);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
