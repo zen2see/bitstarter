b8d85e2ccb2d0b6accbc7e8eabd61ef293fcaea9
Zen2see <ubuntu@ip-172-31-30-88.us-west-2.compute.internal>
2013-07-13 06:02:33 +0000

    Updated web.js with fs.readFileSync

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+#app.get('/', function(request, response) {
+#  response.send('Hello World 2!');
+#});
+
+fs.writeFile('index.html', 'utf8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
