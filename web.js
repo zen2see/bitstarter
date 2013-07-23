0e6791d529901f3f7332754b3572f08d6590269f
Zen2see <ubuntu@ip-172-31-30-88.us-west-2.compute.internal>
2013-07-23 02:28:29 +0000

    Update web.js port

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
-
 var fs = require('fs');
-
 var app = express.createServer(express.logger());
-
 var fsrFS=fs.readFileSync('index.html', 'utf8');
 
- app.get('/', function(request, response) {
-  response.send(fsrFS);
- });
+app.get('/', function(request, response)
+         {
+           response.send(fsrFS);
+         }
+       );
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var port = process.env.PORT || 8080;
+app.listen(port, function()
+            {
+              console.log("Listening on " + port);
+            }
+          );
